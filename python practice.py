6f017442e457704458d4303abab73ddfeba74353
ZeinabHom <91231253+ZeinabHom@users.noreply.github.com>
2021-11-12 09:16:27 -0500

    first commit

---
+++ python practice.py
@@ -0,0 +1,9 @@
+name = "zeinab"
+country = "canada"
+age = 38
+hourly_wage = 16
+satisfied = True
+daily_wage = 8*(hourly_wage)
+
+print("i am "+ name + "\ni am living in "+ country +"\nI am " + str(age) +"\ni have "+ str(8 * hourly_wage ) + "a day\nare you happy? "+ str(satisfied))
+
