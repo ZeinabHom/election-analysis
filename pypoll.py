33dd13ef392dbb7e8b72ee626a2f75900dc1b17d
ZeinabHom <91231253+ZeinabHom@users.noreply.github.com>
2021-11-16 23:06:55 -0500

    Update pypoll.py

--- pypoll.py
+++ pypoll.py
@@ -1,16 +1,24 @@
 #import scv & os module 
 import csv
 import os
+from typing import Counter
 #Generate file path
 file_to_load= os.path.join ('Resources','election_results.csv')
 file_to_save = os.path.join("analysis", "election_analysis.txt")
-
+#define total_votes as a variable with 0 value
+total_votes = 0
 #open file into variable
 with open(file_to_load,newline='') as election_data:
     #convert election_data to CSV  format
     file_reader = csv.reader(election_data)
     #retrieve header of CSV file and put it in header variable
     headers = (next(file_reader))
+
+    #iterate each row of CSV file
+    for row in file_reader:
+        # count the votes
+        total_votes += 1
+
     
     
     # for row in file_reader:
