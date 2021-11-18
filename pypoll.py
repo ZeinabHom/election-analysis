09707d6659b5f440a03a43ae05e7953fba23dced
ZeinabHom <91231253+ZeinabHom@users.noreply.github.com>
2021-11-18 01:50:21 -0500

    Update pypoll.py

--- pypoll.py
+++ pypoll.py
@@ -10,6 +10,8 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 total_votes = 0
 candidate_name = []
 candidate_votes = {}
+county_name = []
+county_votes ={}
 
 #open file into variable
 with open(file_to_load,newline='') as election_data:
@@ -20,27 +22,59 @@ with open(file_to_load,newline='') as election_data:
    
     #iterate each row of CSV file
     for row in file_reader:
-        # count the votes
+                #make county list
+        if row[1] not in county_name:
+            county_name.append(row[1])
+            county_votes [row[1]]=0
+        county_votes[row[1]] += 1
+
+         # count the votes
         total_votes += 1
-        # make a candicates list
+         # make a candicates list
         if row[2] not in candidate_name:
-             candidate_name.append(row[2])
-             candidate_votes[row[2]]=0
+              candidate_name.append(row[2])
+              candidate_votes[row[2]]=0
         candidate_votes[row[2]] += 1
     #find winner with max votes from candidate_votes dictionary
     winner_name = max(candidate_votes, key=candidate_votes.get)
     #find max vote from candidate_votes dictionary
     winner_vote= max(candidate_votes.values())
-        
-   
-# open a text file with write mode
+     
+  
+    # #iterate each row of CSV file
+    # for row in file_reader:
+    #     #make county list
+    #     if row[1] not in county_name:
+    #         county_name.append(row[1])
+    # #print(county_name)
+    #         county_votes [row[1]]=0
+    #     county_votes[row[1]] += 1
+    # print(county_votes)
+            
+    # # #find county winner with max votes from county_vote dictionary
+    county_winner = max(county_name, key=county_votes.get)
+    print(county_winner)
+    # #find max vote from candidate_votes dictionary
+    #county_winner_vote= max(county_votes.values())
+    # #print(county_winner_vote)
+
+
+# # open a text file with write mode
 with open(file_to_save, "w") as txt_file:
-    # write results in  text file   
+     #write results in  text file   
     txt_file.write("Election Results\n")
     txt_file.write("----------------------------\n")
     txt_file.write("Total Votes  " +str(total_votes)+ "\n")
     txt_file.write("----------------------------\n")
+    txt_file.write(f" county votes :\n")
+    for y in county_votes:
+        county_percentage = county_votes[y] /total_votes *100
 
+        txt_file.write(f"{y}: {county_percentage:.1f}% ({county_votes[y]})\n")
+
+    txt_file.write("----------------------------\n")
+    txt_file.write(f"Largest Country turnount :  {county_winner}\n")
+    txt_file.write("----------------------------\n")
     # make empty list for percentages
     max_percentage= []
     #calculate vote percentage of each candidate
@@ -56,3 +90,4 @@ with open(file_to_save, "w") as txt_file:
     txt_file.write(f" Winning Percentage: {max(max_percentage):.1f}%\n")
     txt_file.write("----------------------------\n")
 
+   
